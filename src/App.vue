fdee785d504cf1845d095fa5ed7a1df593abebec
何北木 <583131099@qq.com>
2020-09-16 17:27:20 +0800

    音乐App

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <!-- 视图层 -->
+    <keep-alive>
+      <router-link></router-link>
+    <keep-alive>
+
+    <!-- 音乐控制条 -->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
